aceb438cc529b70a4d8eaf69dd727b1389adc62c
Samuel van Erk <91265950+svanerk@users.noreply.github.com>
2026-09-20 16:21:35 +0200

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// Vul hier de gegevens van je Supabase-project in.
-// Deze file bevat alleen de publieke anon/publishable key.
-// Zet NOOIT een Supabase service_role/secret key in deze website.
-
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://gbhzhiviiqcztzbojrmm.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_b2BnNAZrXn4Gx0HC0fLLxQ_jnPsI-Te"
-};
